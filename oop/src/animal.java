00f561bb796dee27b243c1af88116be382eae6f3
Wizzy645 <130247573+Wizzy645@users.noreply.github.com>
2024-05-26 21:11:47 +0100

    Add files via upload

---
+++ oop/src/animal.java
@@ -0,0 +1,11 @@
+public class animal {
+    String name;
+
+    String move() {
+        return "moves by walking";
+    }
+
+    boolean isAlive() {
+        return true; // Changed "True" to true, as the method should return a boolean value
+    }
+}
